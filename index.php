fa6f3df5e6258974db7bf32b909765e528e936ab
Manuel Maisano <manumaisano@gmail.com>
2024-05-03 16:32:58 +0200

    consegna e add filecensure php

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<!--Creare un form PHP che permetta di inviare due campi: un paragrafo ed una parola da censurare.
+Gestire il tutto con due file diversi.
+Il primo file dovrà permettere all’utente di inserire i dati e inviare la richiesta al server.
+Il secondo file riceverà la richiesta ed eseguirà queste operazioni:
+- stampare a schermo il paragrafo e la sua lunghezza
+- stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare-->
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,6 +21,7 @@
 </head>
 <body>
     <h1>Hello world</h1>
+    <p>ciao mi chiamo <?php echo $nome ?></p>
     
 </body>
 </html>
